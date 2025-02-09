14d7700a4b1f979b9bff2cbefa6ed74ce6fdc9a6
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-02-09 03:02:03 +0100

    Eliminación de Prisma ORM

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,28 +1,8 @@
-import { AuthenticateSchema, RegisterSchema } from "@/validations/auth";
+import { RegisterSchema } from "@/validations/auth";
 import type { AuthenticateState, RegisterState } from "@/interfaces";
 
-async function authenticate(_prevState: AuthenticateState, formData: FormData) {
-    const validatedFields = AuthenticateSchema.safeParse({
-        ...Object.fromEntries(formData.entries())
-    });
-
-    if (!validatedFields.success) {
-        return {
-            errors: validatedFields.error.flatten().fieldErrors,
-            message: 'Missing Fields. Faile to Login User',
-        }
-    }
-
-    try {
-        
-    } catch (error) {
-        return {
-            error,
-            message: 'Database Error: Failed to Login User.'
-        };
-    }
-
-    return {};
+async function authenticate(_prevState: AuthenticateState, _formData: FormData) {
+    
 }
 
 async function register(_prevState: RegisterState, formData: FormData) {
