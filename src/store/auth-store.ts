f03e069c0ca2c70420b01530870f526f43a84e0d
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-02-27 04:03:28 +0100

    Frontend / Modificación del Auth-Store para obtener y manejar el nombre del usuario.

--- src/store/auth-store.ts
+++ src/store/auth-store.ts
@@ -4,6 +4,7 @@ interface State {
     message?: string;
     view: "ERROR" | "SUCESS" | null; 
     userId: string | null;
+    userName: string | null;
     
     register: (name: string, password: string) => Promise<void>;
     login: (name: string, password: string) => Promise<void>;
@@ -14,24 +15,27 @@ interface State {
 
 export const useAuthStore = create<State>()((set) => ({
     userId: null,
+    userName: null,
     view: null,
 
     async register(name, password) {
-        const { ok, message, userId } = await window.ipcRenderer.invoke('user-add', { name, password });
+        const { ok, message, userId, userName } = await window.ipcRenderer.invoke('user-add', { name, password });
         
         set({ 
             view: ok ? "SUCESS" : "ERROR",
             userId,
+            userName,
             message 
         });
     },
 
     async login(name, password) {
-        const { ok, message, userId } = await window.ipcRenderer.invoke('user-verify', { name, password });
+        const { ok, message, userId, userName } = await window.ipcRenderer.invoke('user-verify', { name, password });
 
         set({ 
             view: ok ? "SUCESS" : "ERROR",
             userId,
+            userName,
             message 
         });
     },
@@ -39,17 +43,17 @@ export const useAuthStore = create<State>()((set) => ({
     async logout() {
         await window.ipcRenderer.invoke('session-clear');
 
-        set({ userId: null });
+        set({ userId: null, userName: null });
     },
 
     async checkSession() {
         const session = await window.ipcRenderer.invoke('session-check');
         
         if (!session) {
-            return set({ userId: null });
+            return set({ userId: null, userName: null });
         } 
 
-        set({ userId: session.userId }); 
+        set({ userId: session.userId, userName: session.userName }); 
     },
 
     disableView() {
