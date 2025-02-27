f9c26c358cd6dd63962dd425f4ee4e7a8693d34f
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-02-27 14:46:59 +0100

    Frontend / Maquetación final del componente EmptyData.

--- src/components/webs/EmptyData.tsx
+++ src/components/webs/EmptyData.tsx
@@ -1,7 +1,17 @@
+import { ImFilesEmpty } from "react-icons/im";
+
 export const EmptyData = () => {
   return (
-    <div>
-      <h1>No hay información que mostrar</h1>
+    <div className="flex flex-col h-[550px] items-center">
+      <ImFilesEmpty className="w-40 h-40 mt-28 opacity-40"/>
+      
+      <h1 className="text-2xl font-semibold mt-5">
+        No hay información que mostrar
+      </h1>
+      
+      <p className="text-center mt-2">         
+        No tienes ninguna cuenta web registrada. Puedes agregar una nueva cuenta haciendo clic en el botón de Agregar.
+      </p>
     </div>
   )
 }
